ba7bda38f02f35b371e1a49dbb3f860bdb5599e2
Akhil Sharma <akhils8493@gmail.com>
2023-08-03 19:08:22 +0530

    initial commit

---
+++ yolov5/akhil.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("python detect.py --weights best_5.pt --img 416 --conf 0.65 --source 0")
---
+++ yolov5/run.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("python detect_new.py --weights best_5.pt --img 416 --conf 0.65 --source 0")
